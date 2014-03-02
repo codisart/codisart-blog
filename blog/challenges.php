2a2977f7224914ad3ce04f02ece3fd31c17946e0
test@plop.com <test@plop.com>
2014-03-02 01:03:11 +0100

    Modification de la page Challendes.

--- blog/challenges.php
+++ blog/challenges.php
@@ -4,45 +4,42 @@
 <head>
 	<title>Projets</title>
 	
-	<meta charset="utf-8" />  
+	<meta charset="utf-8" />
 	
 	<link href="styles/general.css" rel="stylesheet" />
 	<link href="styles/projects.css" rel="stylesheet" />
 	
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
 </head>
-	
-	
+		
 <body>
 
 	<div id="global">
-	
-		<?php 				
-			include "blocs/header.php"; 
-		?>
+
+	<?php include "blocs/header.php"; ?>
 
 		<div id="contenu" class="challenges">
-		
+
 			<div>
-        <h1>Le but est de créer des outils pour le plaisir.</h1>
+				<h1>Le but est de créer des outils pour le plaisir.</h1>
 
-        <h2>Soit intégrés au blog soit sur des applis a part entiere.</h2>
-  
-        <p>
-           This is one of the most famous of the sonnets. It is referenced
-           in the film Dead Poets Society and gave names to the band The
-           Darling Buds and the book and television series The Darling Buds
-           of May. Read it and weep!
-        </p>
-    
-        <div id="boutonForm" onclick="">To do</div>
-        
-        <div id="dashboard">
-          
-        </div>
+				<h2>Soit intégrés au blog soit sur des applis a part entiere.</h2>
 
-      </div>
-		
+				<p>
+				This is one of the most famous of the sonnets. It is referenced
+				in the film Dead Poets Society and gave names to the band The
+				Darling Buds and the book and television series The Darling Buds
+				of May. Read it and weep!
+				</p>
+
+				<div id="boutonForm" onclick="">To do</div>
+
+				<div id="dashboard">
+
+				</div>
+
+			</div>
+
 		</div>
 
 	<?php include "blocs/footer.html"; ?>		   
