607f424ea9147a7c3f575a632abdb6e0966759c7
test@plop.com <test@plop.com>
2014-03-02 01:00:07 +0100

    Modification de la page contact

--- blog/contact.php
+++ blog/contact.php
@@ -2,31 +2,31 @@
 <html lang="fr" >
 
 <head> 
-	<title>Contact</title>		
+	<title>Contact</title>
 
-	<meta charset="utf-8" />  
+	<meta charset="utf-8" />
 
 	<link href="styles/general.css" rel="stylesheet" />
 	<link href="styles/contact.css" rel="stylesheet" />
 
 	 <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
 </head>
-		
-    <body>
-		
-		<div id="global">
-			<?php  
-				include "blocs/header.php"; 
-			?>
-			
-			<div id="contenu">		
-				
-				Email :: punkka@gmail.com
-			</div>
-		
-			<?php include "blocs/footer.html"; ?>		
+
+<body>
 	
+	<div id="global">
+		<?php  
+			include "blocs/header.php"; 
+		?>
+		
+		<div id="contenu">
+			
+			Email :: punkka[arobase]gmail.com
 		</div>
+	
+		<?php include "blocs/footer.html"; ?>
+
+	</div>
 				
-	</body>
+</body>
 </html>
