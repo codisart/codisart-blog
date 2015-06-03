234635fd75d52d1d614e26347149c1760ec06ed1
punkka <louis.celier@gmail.com>
2015-06-03 14:47:17 +0200

    [PATCH] Corrections code style.

--- src/blog/nexus/main.php
+++ src/blog/nexus/main.php
@@ -5,48 +5,42 @@
     define('REPARTITION', 15);
 
 
-
-
-
 	/***** Base de données *****/
 	
 	define("SERVER","mysql:host=localhost;dbname=blog");
 	define("USER", "root");
 	define("PASS", "");
 	
-	function connexionBDD()
-	{
-		try
-		{
+	function connexionBDD() {
+
+		try {
 			$connexionBDD = new \PDO(SERVER, USER, PASS);
 			$connexionBDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 			$connexionBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);			
 		}
-		catch (Exception $e)
-		{
-				die('Erreur : ' . $e->getMessage());
+		catch (Exception $e) {
+    		echo 'Connexion échouée : ' . $e->getMessage();
+    		return NULL;
 		}
 			
 		return $connexionBDD;
 	}
 	
+
 	/***** Classes & Librairies*****/
 	
 	define("CLASSES",__DIR__."/classes");
 	define("LIBS",__DIR__."/librairies");	
 
-    function loadFile($file)
-    {
-		if(is_file(CLASSES."/$file.class.php"))
-		{
+    function loadFile($file) {
+
+		if(is_file(CLASSES."/$file.class.php")) {
 			require CLASSES."/$file.class.php";
 		}
 		
-		if(is_file(LIBS."/$file.lib.php"))
-		{
+		if(is_file(LIBS."/$file.lib.php")) {
 			 require LIBS."/$file.lib.php";
 		}
     }
     
     spl_autoload_register("loadFile");
-
--- src/blog/nexus/main_server_blog.php
+++ src/blog/nexus/main_server_blog.php
@@ -6,36 +6,34 @@
 	define("USER", "punkka_blog");
 	define("PASS", "wcGRJZ9LHQpnbXK8");
 	
-	function connexionBDD()
-	{
-		try
-		{
-			$connexionBDD = new PDO(SERVER, USER, PASS);
+	function connexionBDD() {
+
+		try {
+			$connexionBDD = new \PDO(SERVER, USER, PASS);
 			$connexionBDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			$connexionBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+			$connexionBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);			
 		}
-		catch (Exception $e)
-		{
-				die('Erreur : ' . $e->getMessage());
+		catch (Exception $e) {
+    		echo 'Connexion échouée : ' . $e->getMessage();
+    		return NULL;
 		}
 			
 		return $connexionBDD;
-	}	
+	}
 	
+
 	/***** Classes & Librairies*****/
 	
 	define("CLASSES",__DIR__."/classes");
 	define("LIBS",__DIR__."/librairies");	
 
-    function loadFile($file)
-    {
-		if(is_file(CLASSES."/$file.class.php"))
-		{
+    function loadFile($file) {
+
+		if(is_file(CLASSES."/$file.class.php")) {
 			require CLASSES."/$file.class.php";
 		}
 		
-		if(is_file(LIBS."/$file.lib.php"))
-		{
+		if(is_file(LIBS."/$file.lib.php")) {
 			 require LIBS."/$file.lib.php";
 		}
     }
