06e95efb52380182ec95f29859f8ffdf460c2730
codisart <louis.celier@gmail.com>
2015-08-26 15:24:29 +0200

    [STYLE] Removal inused commented code.

--- src/blog/admin/commentaires.php
+++ src/blog/admin/commentaires.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	if (!isset($_SESSION)){session_start();}
 	if (!isset($_SESSION['login'])){require 'connexion.php';exit();}
 ?>
@@ -6,22 +6,22 @@
 <!DOCTYPE html>
 <html lang="fr" >
 
-<head> 
+<head>
 	<meta charset="utf-8" />
 	<title>CodisArt - Back Office - Commentaires</title>
-		
-	<link rel="stylesheet" href="css/admin.css" />	
+
+	<link rel="stylesheet" href="css/admin.css" />
 	<link rel="shortcut icon" type="image/x-icon" href="images/admin_favicon.ico" />
 </head>
-		
+
 <body>
-		
+
 	<div id="global">
-	
+
 		<header class="header">
 			<h1>Bienvenue sur le back-office du blog !</h1>
 		</header>
-		
+
 		<nav class="nav">
 			<ul>
 				<li class="tab bg-light-blue"><a class="option" href="index.php">Articles</a></li></li>
@@ -30,64 +30,61 @@
 				<li class="tab bg-light-blue"><a class="option" href="divers.php">Divers</a></li>
 				<li class="tab bg-light-blue"><a class="option" href="deconnexion.php">Se déconnecter</a></li>
 			</ul>
-		</nav>	
+		</nav>
 
-		
+
 		<div class="contenu grand">
 
 			<table class="workspace">
-					
+
 				<tr>
 				   <th class="pseudo">Pseudo</th>
 				   <th class="date">date</th>
 				   <th class="commentaires">Message</th>
 				   <th class="operations">operations</th>
 			   </tr>
-				   
-				<?php					
+
+				<?php
 					while(empty($directories)){chdir('..'); $directories = glob('nexus');}
 					require_once(getcwd().'/nexus/main.php');
 
 					$controller = Controller::getInstance();
 					$controller->recoverGET('id_article');
 
-					// if(!controlNumber($page)){ $page = 1;}
-					
-					$article = new Article($id_article);		
-					
+					$article = new Article($id_article);
+
 					$comments = $article->getAllCommentaires();
 
 					foreach ($comments as $comment):
-			
+
 				?>
-					
-				<tr class="commentaire" id="commentaire<?php echo $comment->id; ?>"> 
+
+				<tr class="commentaire" id="commentaire<?php echo $comment->id; ?>">
 
 					<td class="pseudo"><?php echo $comment->pseudo; ?></td>
 
 					<td class="date"><?php echo $comment->getDateOn2Rows(); ?></td>
-		
+
 					<td class="content"><?php echo $comment->contenu; ?></td>
-					
+
 					<td class="operations">
 						<img src="images/delete.png" onclick="supprimerCommentaire('<?php echo $comment->id; ?>');" title="Supprimer"/>
 					</td>
-					
-				</tr>			
-					
-				<?php						
-					endforeach;		
-				?>
-				
-			</table>		
 
-		</div>		
-			
+				</tr>
+
+				<?php
+					endforeach;
+				?>
+
+			</table>
+
+		</div>
+
 	</div>
-	
-	
-	<script src="script.js"></script>		
+
+
+	<script src="script.js"></script>
 </body>
 
 </html>
-	
