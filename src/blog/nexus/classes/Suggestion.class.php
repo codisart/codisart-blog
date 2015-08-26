9a8a231d901b92872e3d9a71781ef6e1c8685d1f
codisart <louis.celier@gmail.com>
2015-08-26 13:38:10 +0200

    [BUG] Critical : syntax error in the Suggestion class.

--- src/blog/nexus/classes/Suggestion.class.php
+++ src/blog/nexus/classes/Suggestion.class.php
@@ -1,72 +1,72 @@
 <?php
-	
+
 	class Suggestion {
-		
+
 		// private $page;
 		// private $adresse;
 		// private $nombreMessagesPage;
 		// private $viewMessage;
 		// private $messages;
-		
+
 		public function __construct($id, $pseudo = '', $mail = '', $date = 0, $message = '') {
 			$this->id = $id;
-			
-			if ($pseudo == '' && $mail == '' && $date == 0 && $message == '') {				
+
+			if ($pseudo == '' && $mail == '' && $date == 0 && $message == '') {
 				$connexionBDD = connexionBDD();
-				
+
 				$requete = $connexionBDD->query("
 					SELECT id, pseudo, mail, date, message
-					FROM messages 
+					FROM messages
 					WHERE id='$id'
-					ORDER BY id DESC 
+					ORDER BY id DESC
 					LIMIT 0,1
-				");			
-			
+				");
+
 				$donnees = $requete->fetch();
-				
+
 				$pseudo = $donnees['pseudo'];
 				$mail = $donnees['mail'];
 				$date = $donnees['date'];
 				$message = $donnees['message'];
-				
-				unset($connexionBDD);		
+
+				unset($connexionBDD);
 			}
-			
+
 			$this->pseudo = $pseudo;
 			$this->mail = $mail;
 			$this->date = $date;
 			$this->message = $message;
 		}
-		
+
 		// Methode Magique
-		
+
 		/**
 		 *  Permet la lecture seule des membres
-		 */		
+		 */
 		public function __get ($nom) {
             if (isset($this->$nom)) {
 				return $this->$nom;
 			}
 			return "<p class=\"error\">Impossible d'accéder à l'attribut <strong>$nom</strong>, désolé !</p>";
         }
-		
+
 		/**
 		 *	@return Collection les messages de la page demandée.
-		 */		
-		public function getCommentaires($page, $nombreMessagesPage = 10) {			 
+		 */
+		public function getCommentaires($page, $nombreMessagesPage = 10) {
 			$this->page = $page;
 			$this->nombreMessagesPage = $nombreMessagesPage;
-			
+
 			$this->messages = new Collection();
-			
+
 			$limit = ($this->page -1)*$this->nombreMessagesPage;
-						
+
 			$requete = connexionBDD()->query("SELECT id, pseudo, date, mail, message
-															FROM messages 
-															ORDER BY id DESC 
+															FROM messages
+															ORDER BY id DESC
 															LIMIT $limit, $nombreMessagesPage");
 
-								
+
 			if (false === ($donnees = $requete->fetch())) {
 				--$page;
 
@@ -74,38 +74,38 @@
 					echo '<h5 class="error">Il n\'y a pas d\'articles sélectionnés</h5>';
 					$this->nombreMessagesPage = 0;
 				}
-				
-				header('Location : '.$this->adresse.'?page='.$page);				
+
+				header('Location : '.$this->adresse.'?page='.$page);
 			}
 
 			do {
-				$this->messages[$donnees['id']] = new Commentaire($donnees['id'], $donnees['pseudo'], $donnees['date'], $donnees['message']);							
+				$this->messages[$donnees['id']] = new Commentaire($donnees['id'], $donnees['pseudo'], $donnees['date'], $donnees['message']);
 			} while ($donnees = $requete->fetch());
-			
-			return $this->messages;
-		}		
-				
-		
-		
 
-		// Fonctions / Méthodes		
+			return $this->messages;
+		}
+
+
+
+
+		// Fonctions / Méthodes
 		public static function ajouter($pseudo, $mail, $contenu) {
-			//TODO add exception 
+			// @TODO add exception
 			if ($pseudo != "" && $mail != "" && $contenu !="") {
 				$requete = $connexionBDD()->prepare("INSERT INTO messages (pseudo, mail, message) VALUES (?, ?, ?)");
-				
-				return !$requete->execute(array($pseudo, $mail, $contenu)));			
+
+				return !$requete->execute(array($pseudo, $mail, $contenu));
 			}
 
 			echo '<h5 class="error">Vous n\'avez pas rempli correctement  le formulaire</h5>';
 			return false;
 		}
-		
+
 		public function supprimer() {
 			$id = $this->id;
-					
+
 			$connexionBDD = connexionBDD();
-				
+
 			if (!$connexionBDD->query("DELETE FROM messages WHERE id ='$id'")) {
 				echo 'Ce message a déjà été supprimé ou n\'existe pas !';
 			}
@@ -118,5 +118,5 @@
 
 			return str_replace(' ', "\n", $date);
 		}
-			
+
 	}
