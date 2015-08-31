e7e8b7f7aecc4b1d6a1ff3b51a7d11e8558f8ccb
codisart <louis.celier@gmail.com>
2015-08-31 18:52:01 +0200

    [FEATURE] Mise en place d'un fichier de config par défault et d'un ignore sur le fichier de config local.

--- src/blog/nexus/main.php
+++ src/blog/nexus/main.php
@@ -1,45 +1,38 @@
-<?php 
+<?php
 
-    /***** Config *****/
+	include('config.php');
+	require('default.config.php');
 
-    define('REPARTITION', 15);
 
-
-	/***** Base de données *****/
-	
-	define("SERVER", "mysql:host=localhost;dbname=blog");
-	define("USER", "root");
-	define("PASS", "");
-	
 	function connexionBDD() {
 
 		try {
 			$connexionBDD = new \PDO(SERVER, USER, PASS);
 			$connexionBDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			$connexionBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);			
+			$connexionBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		} catch (Exception $e) {
     		echo 'Connexion échouée : ' . $e->getMessage();
-    		return NULL;
+    		return null;
 		}
-			
+
 		return $connexionBDD;
 	}
-	
+
 
 	/***** Classes & Librairies*****/
-	
-	define("CLASSES", __DIR__."/classes");
-	define("LIBS", __DIR__."/librairies");	
+
+	define("CLASSES",__DIR__."/classes");
+	define("LIBS",__DIR__."/librairies");
 
     function loadFile($file) {
 
 		if (is_file(CLASSES."/$file.class.php")) {
 			require CLASSES."/$file.class.php";
 		}
-		
+
 		if (is_file(LIBS."/$file.lib.php")) {
 			 require LIBS."/$file.lib.php";
 		}
     }
-    
+
     spl_autoload_register("loadFile");
