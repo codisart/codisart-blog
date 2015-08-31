6f51e5f918b2fe31881bf831552a2ff066b3039f
codisart <louis.celier@gmail.com>
2015-08-31 18:52:50 +0200

    [EDIT] [FEATURE] Mise en place d'un fichier de config par défault et d'un ignore sur le fichier de config local.

---
+++ src/blog/nexus/default.config.php
@@ -0,0 +1,11 @@
+<?php
+
+/***** Config *****/
+
+defined('REPARTITION') or define('REPARTITION', 15);
+
+/***** Base de données *****/
+
+defined('SERVER') or define("SERVER","mysql:host=mysql.alwaysdata.com;dbname=punkka_blog");
+defined('USER') or define("USER", "punkka_blog");
+defined('PASS') or define("PASS", "wcGRJZ9LHQpnbXK8");
