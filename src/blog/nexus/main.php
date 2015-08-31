f5d9cf5dbff343e3aa79fd5ae89acd64a5541af4
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-08-31 16:56:08 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/blog/nexus/main.php
+++ src/blog/nexus/main.php
@@ -11,8 +11,8 @@
 			$connexionBDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 			$connexionBDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		} catch (Exception $e) {
-    		echo 'Connexion échouée : ' . $e->getMessage();
-    		return null;
+			echo 'Connexion échouée : ' . $e->getMessage();
+			return null;
 		}
 
 		return $connexionBDD;
@@ -21,10 +21,10 @@
 
 	/***** Classes & Librairies*****/
 
-	define("CLASSES",__DIR__."/classes");
-	define("LIBS",__DIR__."/librairies");
+	define("CLASSES", __DIR__."/classes");
+	define("LIBS", __DIR__."/librairies");
 
-    function loadFile($file) {
+	function loadFile($file) {
 
 		if (is_file(CLASSES."/$file.class.php")) {
 			require CLASSES."/$file.class.php";
@@ -33,6 +33,6 @@
 		if (is_file(LIBS."/$file.lib.php")) {
 			 require LIBS."/$file.lib.php";
 		}
-    }
+	}
 
-    spl_autoload_register("loadFile");
+	spl_autoload_register("loadFile");
