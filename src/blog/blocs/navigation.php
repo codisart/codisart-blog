40805cbecccf20a5e0a5099b75e7b92a19c98269
codisart <louis.celier@gmail.com>
2015-09-01 18:21:43 +0200

    [BOG] Corection de l'affichage du retour à la première page.

--- src/blog/blocs/navigation.php
+++ src/blog/blocs/navigation.php
@@ -1,9 +1,15 @@
 <?php
 	if ($nombreArticles != 0) {
-
-		if ($page > 1 && $page <= $maxPages) { ?>
+		//@TODO l'url est fausse pour les archives et la recherche 
+		if ($page > 1 && $page < $maxPages) { ?>
             <div  style="float:left">
                 <a href="index.php?page=<?php echo $page-1; echo $nombreArticles === 10 ? '': '&n='.$nombreArticles; ?>">Recents articles</a>
+            </div>
+        <?php }
+
+		if ($page == $maxPages) { ?>
+            <div  style="float:left">
+                <a href="index.php?page=1">Retour à la première page</a>
             </div>
         <?php }
 
@@ -13,11 +19,6 @@
             </div>
         <?php }
 
-		if ($page === $maxPages) { ?>
-            <div  style="float:left">
-                <a href="index.php?page=1">Retour à la première page</a>
-            </div>
-        <?php }
 
 	}
 ?>
