713908686550e91430a5ceebbcef1c136d203690
codisart <louis.celier@gmail.com>
2015-09-02 17:17:59 +0200

    [DEPLOY] reflexions sur un deploiement simple.

---
+++ deploy/prod.sh
@@ -0,0 +1,7 @@
+
+
+git diff --name-only 40805cb f46e49e
+
+sftp -oStrictHostKeyChecking=no punkka@ssh-punkka.alwaysdata.net
+
+ wget -q https://github.com/codisart/codisart-blog/raw/master/.gitignore --no-check-certificate
