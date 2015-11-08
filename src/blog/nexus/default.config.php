115ea7ebadaab46ce59e5ca8c63c9c645222c76a
codisart <louis.celier@gmail.com>
2015-11-08 22:36:41 +0100

    [DOC] Corrections diverses

--- src/blog/nexus/default.config.php
+++ src/blog/nexus/default.config.php
@@ -5,7 +5,3 @@
 defined('REPARTITION') or define('REPARTITION', 15);
 
 /***** Base de données *****/
-
-defined('SERVER') or define("SERVER", "mysql:host=mysql.alwaysdata.com;dbname=punkka_blog");
-defined('USER') or define("USER", "punkka_blog");
-defined('PASS') or define("PASS", "wcGRJZ9LHQpnbXK8");
