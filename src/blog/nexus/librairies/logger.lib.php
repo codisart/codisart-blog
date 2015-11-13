ceb469b35ab4150921fa6b78750e99b64baca9af
codisart <louis.celier@gmail.com>
2015-11-13 11:36:11 +0100

    [BUG] Change the visibility of logger construct method

--- src/blog/nexus/librairies/logger.lib.php
+++ src/blog/nexus/librairies/logger.lib.php
@@ -1,24 +1,21 @@
 <?php
-	
+
 class Logger {
 	private static $instance = null;
-	
-	private function __construct() {
-		
+
+	protected function __construct() {
+
 	}
 
 	private static function getInstance() {
 		if (is_null(self::$instance)) {
 			self::$instance = new Logger();
 		}
-		
+
 		return self::$instance;
 	}
-	
+
 	public static function recordAction() {
 		self::getInstance();
 	}
 }
-	
-	
-	
