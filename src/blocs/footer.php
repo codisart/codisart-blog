e8804b6bdc037b412d4fc4a3b90027de3ca1ae0e
codisart <louis.celier@gmail.com>
2016-01-11 14:56:10 +0100

    [TEST] footer.php

--- src/blocs/footer.php
+++ src/blocs/footer.php
@@ -1,10 +1,1 @@
-﻿<div class="footer">
-	<h3>© Punkka</h3>
-	<h4>Since 2010</h4>
-	
-	<div>
-		<a href="contact.php">Contacts</a> - 
-		<a href="admin/">Admin</a>
-	</div>
-</div>
-		
+<div class="footer"><h3>© Punkka</h3><h4>Since 2010</h4><div><a href="contact.php">Contacts</a>-<a href="admin/">Admin</a></div></div>
