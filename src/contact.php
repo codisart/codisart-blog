81bb47535d971416ef08c30a84375bc392d59b35
codisart <louis.celier@gmail.com>
2018-04-02 01:22:44 +0200

    Refacto contact page with templating

--- src/contact.php
+++ src/contact.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr" >
+<?php
 
-<head>
-	<title>Contact</title>
+require_once('nexus/main.php');
 
-	<meta charset="utf-8" />
-
-	<link href="css/general.css" rel="stylesheet" />
-
-	 <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
-</head>
-
-<?php require_once('nexus/main.php'); ?>
-<body>
-
-	<div id="global">
-		<?=$templates->render('header') ?>
-
-		<div id="contenu">
-			Email :: punkka[arobase]gmail.com
-		</div>
-
-		<?=$templates->render('footer') ?>
-	</div>
-
-</body>
-</html>
+echo $templates->render('pages/contact', [
+    'title' => 'Contact'
+]);
---
+++ src/templates/pages/contact.php
@@ -0,0 +1,7 @@
+<?php $this->layout('layouts/front', ['title' => 'Contact']) ?>
+
+<?= $this->insert('subtitle', ['subtitle' => 'How to contact me ?']) ?>
+
+<a id="retourArticle" href="index.php">Retour à la liste des articles</a>
+
+<div>Email :: punkka[arobase]gmail.com</div>
